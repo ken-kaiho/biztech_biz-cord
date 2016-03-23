41007cbc06d5c7879e111057ed2d431121aaa67a
genki-kase <horizontal.genki3317@gmail.com>
2016-03-23 07:45:45 -0400

    祝 初回コミット

---
+++ src/main/java/com/k3/BizCordApplication.java
@@ -0,0 +1,12 @@
+package com.k3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizCordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizCordApplication.class, args);
+	}
+}
---
+++ src/test/java/com/k3/BizCordApplicationTests.java
@@ -0,0 +1,18 @@
+package com.k3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BizCordApplication.class)
+@WebAppConfiguration
+public class BizCordApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
