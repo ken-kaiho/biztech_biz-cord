2ee0a51c0588caba64e81088864f34607417d7dc
takuya kubo <tkuboma@gmail.com>
2016-04-07 21:32:49 +0900

    User select

--- src/main/java/com/k4/BizCordApplication.java
+++ src/main/java/com/k4/BizCordApplication.java
@@ -1,15 +1,37 @@
 package com.k4;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.k4.dao.UserDao;
+import com.k4.entity.User;
+
+@RestController
 @SpringBootApplication
 public class BizCordApplication {
 
+	@Autowired
+	UserDao userDao;
+	
 	public static void main(String[] args) {
 		System.out.println ("unko!");
 		
 		
 		SpringApplication.run(BizCordApplication.class, args);
 	}
+	
+	public List<User> selectAll(){
+		return userDao.selectAll();
+	}
+	
+	@RequestMapping("/")
+	public List<User> chinko(){
+		return userDao.selectAll();
+	}
+	
 }
---
+++ src/main/java/com/k4/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.k4.dao;
+
+import java.util.List;
+
+import org.seasar.doma.Dao;
+import org.seasar.doma.Select;
+import org.seasar.doma.boot.ConfigAutowireable;
+
+import com.k4.entity.User;
+
+@Dao
+@ConfigAutowireable
+public interface UserDao {
+    @Select
+    public List<User> selectAll();
+}
