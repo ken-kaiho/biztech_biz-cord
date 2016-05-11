8ad7f58a584ccb3914d9826f8f43312a41016230
genki-kase <horizontal.genki3317@gmail.com>
2016-05-11 19:21:31 +0900

    add doma2 setting.

--- src/main/java/com/k4/BizCordApplication.java
+++ src/main/java/com/k4/BizCordApplication.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.k4.dao.UserDao;
@@ -35,9 +37,17 @@ public class BizCordApplication {
 		return userDao.selectAll();
 	}
 	
-	@RequestMapping("/")
+	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public List<User> chinko(){
 		return userDao.selectAll();
+	}
+	
+	@RequestMapping(path = "/", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON})
+	public Map<String, Integer> addUser(@RequestBody User user) {
+		int id = userDao.insert(user);
+		Map<String, Integer> data = new HashMap<>();
+		data.put("id", id);
+		return data;
 	}
 	
 	@RequestMapping(path = "/circleElem", produces = { MediaType.APPLICATION_JSON })
